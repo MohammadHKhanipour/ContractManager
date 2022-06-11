3eba060f7eb83d4994d6b67e70ffb9c97bdfb3e6
Mohammad Hassan Khanipour <mohammadhkhanipour@outlook.com>
2022-06-11 13:11:00 +0430

    Add Context

---
+++ ContractManager.Infrastructure/Migrations/20220611083948_V2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContractManager.Infrastructure.Migrations
+{
+    public partial class V2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "Correspondences",
+                newName: "CorrespondenceContent");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CorrespondenceContent",
+                table: "Correspondences",
+                newName: "Content");
+        }
+    }
+}
